22b6c295f66c28dd4c4fcb2db1bd9f2a9e5f3590
Sam Van Battel <teacher.svb@gmail.com>
2020-11-16 20:38:41 +0100

    +

---
+++ firebase-conf.js
@@ -0,0 +1,16 @@
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyCM_XMS39wXtZ3QLoYzHx-dEVPUqiNmQeM",
+    authDomain: "brouwerij-app.firebaseapp.com",
+    databaseURL: "https://brouwerij-app.firebaseio.com",
+    projectId: "brouwerij-app",
+    storageBucket: "brouwerij-app.appspot.com",
+    messagingSenderId: "23491668877",
+    appId: "1:23491668877:web:9c7a210cad3c856a35abc4",
+    measurementId: "G-Q78PN4E483"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
