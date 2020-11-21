29788b1d8f35d2b63e23c1b65f3055bbf8455062
Sam Van Battel <teacher.svb@gmail.com>
2020-11-21 09:48:24 +0100

    Update firebase-io.js

--- scripts/firebase-io.js
+++ scripts/firebase-io.js
@@ -39,131 +39,132 @@ function readRecipeFromFirebase() {
 
 
 function saveToFirebase() {
-    var recipe = {
-        naam: "Tripel Pils Nottingham",
-        water: 15,
-        spoelwater: 10,
-        koeltemp: 22,
-        mout: [
-            {
-                naam: "Pilsmout",
-                hoeveelheid: 5000
-            }
-        ],
-        gist: "Danstar Nottingham",
-        maischschema: [
-            {
-                temp: 62,
-                tijd: 30
-            },
-            {
-                temp: 72,
-                tijd: 60
-            },
-            {
-                temp: 78,
-                tijd: 5
-            }
-        ],
-        kookschema: [
-            {
-                tijd: 60,
-                toevoegen: [
-                    {
-                        naam: "Nothern Brewer hop",
-                        hoeveelheid: 20,
-                        eenheid: "gram"
-                    }
-                ]
-            },
-            {
-                tijd: 15,
-                toevoegen: [
-                    {
-                        naam: "Saaz hop",
-                        hoeveelheid: 20,
-                        eenheid: "gram"
-                    }
-                ]
-            },
-            {
-                tijd: 10,
-                toevoegen: [
-                    {
-                        naam: "Nothern Brewer",
-                        hoeveelheid: 20,
-                        eenheid: "gram"
-                    }
-                ]
-            }
-        ]
-    };
+    let JSONrecipes = [
+        {
+            "naam": "Tripel Pils Nottingham",
+            "water": 15,
+            "spoelwater": 10,
+            "koeltemp": 22,
+            "mout": [
+                {
+                    "naam": "Pilsmout",
+                    "hoeveelheid": 5000
+                }
+            ],
+            "gist": "Danstar Nottingham",
+            "maischschema": [
+                {
+                    "temp": 62,
+                    "tijd": 30
+                },
+                {
+                    "temp": 72,
+                    "tijd": 60
+                },
+                {
+                    "temp": 78,
+                    "tijd": 5
+                }
+            ],
+            "kookschema": [
+                {
+                    "tijd": 60,
+                    "toevoegen": [
+                        {
+                            "naam": "Nothern Brewer",
+                            "hoeveelheid": 20,
+                            "eenheid": "gram",
+                            "type": "hop"
+                        }
+                    ]
+                },
+                {
+                    "tijd": 15,
+                    "toevoegen": [
+                        {
+                            "naam": "Saaz",
+                            "hoeveelheid": 20,
+                            "eenheid": "gram",
+                            "type": "hop"
+                        }
+                    ]
+                },
+                {
+                    "tijd": 10,
+                    "toevoegen": [
+                        {
+                            "naam": "Suiker",
+                            "hoeveelheid": 250,
+                            "eenheid": "gram",
+                            "type": "suiker"
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            "naam": "Weizen Pils-Tarwe Weihenstephan",
+            "water": 15,
+            "spoelwater": 10,
+            "koeltemp": 22,
+            "mout": [
+                {
+                    "naam": "Pilsmout",
+                    "hoeveelheid": 2000
+                },
+                {
+                    "naam": "Tarwemout",
+                    "hoeveelheid": 2500
+                }
+            ],
+            "gist": "Wyeast 3068 Weihenstephan Weizen",
+            "maischschema": [
+                {
+                    "temp": 40,
+                    "tijd": 20
+                },
+                {
+                    "temp": 50,
+                    "tijd": 15
+                },
+                {
+                    "temp": 62,
+                    "tijd": 30
+                },
+                {
+                    "temp": 72,
+                    "tijd": 30
+                },
+                {
+                    "temp": 78,
+                    "tijd": 5
+                }
+            ],
+            "kookschema": [
+                {
+                    "tijd": 60,
+                    "toevoegen": [
+                        {
+                            "naam": "Hallertau Perle",
+                            "hoeveelheid": 20,
+                            "eenheid": "gram",
+                            "type": "hop"
+                        }
+                    ]
+                }
+            ]
+        }
+    ];
 
-    firebase.database().ref('recipes').child(recipe.naam).set(recipe)
-        .then(function(snapshot) {
-            //success(); // some success method
-        }, function(error) {
-            console.log('error' + error);
-            //error(); // some error method
-        });
-    
-    var recipe = {
-        naam: "Weizen Pils-Tarwe Weihenstephan",
-        water: 15,
-        spoelwater: 10,
-        koeltemp: 22,
-        mout: [
-            {
-                naam: "Pilsmout",
-                hoeveelheid: 2000
-            },
-            {
-                naam: "Tarwemout",
-                hoeveelheid: 2500
-            }
-        ],
-        gist: "Wyeast 3068 Weihenstephan Weizen",
-        maischschema: [
-            {
-                temp: 40,
-                tijd: 20
-            },
-            {
-                temp: 50,
-                tijd: 15
-            },
-            {
-                temp: 62,
-                tijd: 30
-            },
-            {
-                temp: 72,
-                tijd: 30
-            },
-            {
-                temp: 78,
-                tijd: 5
-            }
-        ],
-        kookschema: [
-            {
-                tijd: 60,
-                toevoegen: [
-                    {
-                        naam: "Hallertau Perle",
-                        hoeveelheid: 20,
-                        eenheid: "gram"
-                    }
-                ]
-            }
-        ]
-    };
+    for (let i = 0; i < JSONrecipes.length; ++i) {
+        let JSONrecipe = JSONrecipes[i];
 
-    firebase.database().ref('recipes').child(recipe.naam).set(recipe)
-        .then(function(snapshot) {
-            //success(); // some success method
-        }, function(error) {
-            console.log('error' + error);
-            //error(); // some error method
-        });
+        firebase.database().ref('recipes').child(JSONrecipe.naam).set(JSONrecipe)
+            .then(function(snapshot) {
+                //success(); // some success method
+            }, function(error) {
+                console.log('error' + error);
+                //error(); // some error method
+            });
+    }
 }
