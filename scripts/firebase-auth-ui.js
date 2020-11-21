79ec6b4fe53e39de761d0affe6e4f1546e80c01a
Sam Van Battel <teacher.svb@gmail.com>
2020-11-21 22:45:11 +0100

    Update firebase-auth-ui.js

--- scripts/firebase-auth-ui.js
+++ scripts/firebase-auth-ui.js
@@ -8,36 +8,46 @@ ui.start('#firebaseui-auth-container', {
     // Other config options...
 });*/
 
-var ui = firebaseui.auth.AuthUI.getInstance() || new firebaseui.auth.AuthUI(firebase.auth());
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        console.log("succes");
+        document.querySelector("header>h1").style.display = 'none';
+        document.querySelector("header>h2").style.display = 'none';
+        document.querySelector("header>form#editRecipe").style.display = 'block';
+    } else {
 
-var uiConfig = {
-    callbacks: {
-        signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-            // User successfully signed in.
-            // Return type determines whether we continue the redirect automatically
-            // or whether we leave that to developer to handle.
-            console.log("succes");
-            document.querySelector("header>h1").style.display = 'none';
-            document.querySelector("header>h2").style.display = 'none';
-            document.querySelector("header>form#editRecipe").style.display = 'block';
-            return false;
-        },
-        uiShown: function() {
-        // The widget is rendered. Hide the loader.
-        document.getElementById('loader').style.display = 'none';
-        }
-    },
-    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-    signInFlow: 'popup',
-    signInSuccessUrl: '<url-to-redirect-to-on-success>',
-    signInOptions: [
-        // Leave the lines as is for the providers you want to offer your users.
-        firebase.auth.GoogleAuthProvider.PROVIDER_ID
-    ],
-    // Terms of service url.
-    tosUrl: '<your-tos-url>',
-    // Privacy policy url.
-    privacyPolicyUrl: '<your-privacy-policy-url>'
-};
-
-ui.start('#firebaseui-auth-container', uiConfig);
+        var ui = firebaseui.auth.AuthUI.getInstance() || new firebaseui.auth.AuthUI(firebase.auth());
+        
+        var uiConfig = {
+            callbacks: {
+                signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+                    // User successfully signed in.
+                    // Return type determines whether we continue the redirect automatically
+                    // or whether we leave that to developer to handle.
+                    console.log("succes");
+                    document.querySelector("header>h1").style.display = 'none';
+                    document.querySelector("header>h2").style.display = 'none';
+                    document.querySelector("header>form#editRecipe").style.display = 'block';
+                    return false;
+                },
+                uiShown: function() {
+                // The widget is rendered. Hide the loader.
+                document.getElementById('loader').style.display = 'none';
+                }
+            },
+            // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+            signInFlow: 'popup',
+            signInSuccessUrl: '<url-to-redirect-to-on-success>',
+            signInOptions: [
+                // Leave the lines as is for the providers you want to offer your users.
+                firebase.auth.GoogleAuthProvider.PROVIDER_ID
+            ],
+            // Terms of service url.
+            tosUrl: '<your-tos-url>',
+            // Privacy policy url.
+            privacyPolicyUrl: '<your-privacy-policy-url>'
+        };
+        
+        ui.start('#firebaseui-auth-container', uiConfig);
+    }
+  });
