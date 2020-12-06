50c12d809ac64563cc29fff3a4c39465e1ccbd46
Sam Van Battel <teacher.svb@gmail.com>
2020-12-06 16:16:06 +0100

    update

--- scripts/brouwen.js
+++ scripts/brouwen.js
@@ -111,7 +111,7 @@ function fillForm(JSONrecipe) {
         return;
     }
 
-    document.querySelector("form#editRecipe").jsonForm({
+    $("form#editRecipe").jsonForm({
         "schema": {
             "naam": {
                 "type": "string"
